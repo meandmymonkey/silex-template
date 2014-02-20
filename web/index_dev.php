ef5e690e4e480cf944fcd550947a6acfa515c546
Andreas Hucks <andreas.hucks@sensiolabs.de>
2014-02-20 18:44:11 +0100

    added security checks to dev and test front controllers

--- web/index_dev.php
+++ web/index_dev.php
@@ -3,6 +3,14 @@
 use Silex\Application;
 use Symfony\Component\Debug\Debug;
 
+if (isset($_SERVER['HTTP_CLIENT_IP'])
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
+) {
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file.');
+}
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 Debug::enable();
